fdb74ffbd072f83cc881b544d0974bed92c75949
shreyasharmaweb <shreyasharma7051@gmail.com>
2023-09-09 17:24:17 +0530

    kjndskjvnkjndvkj

---
+++ classroom/main.js
@@ -0,0 +1,12 @@
+// console.log("I am shreya sharma")
+// let a=10;
+// let b=20;
+// console.log(a+b);
+// const num1=parseInt(prompt("enter the numer1"));
+// const num2=parseInt(prompt("enter the numer2"));
+// alert(num1+num2);
+function Calc()
+{
+    var a=document.getElementById("num").value ;
+    document.getElementById("num").value=(a*a) ;
+}
